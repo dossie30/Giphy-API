9bcfa63330f3d6b0f2571e66ee764247d5b66809
Dawn Tossie <dawn.tossie@gmail.com>
2018-03-13 18:22:46 -0400

    <working animate>

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -74,12 +74,15 @@ $(document).ready(function () {
 
             // Creating an image tag
             var personImage = $("<img>");
-            var personImageAnimate = $("<img2>");
+            // var personImageAnimate = $("<img2>");
 
             // Giving the image tag an src attribute of a proprty pulled off the
             // result item
+            personImage.attr("data-still", results[i].images.fixed_height_still.url);
+            personImage.attr("data-animate", results[i].images.fixed_height.url);
+            personImage.attr("data-state","still");
             personImage.attr("src", results[i].images.fixed_height_still.url);
-            personImageAnimate.attr("src", results[i].images.fixed_height.url);
+            // personImageAnimate.attr("src", results[i].images.fixed_height.url);
 
             // Appending the paragraph and personImage we created to the "gifDiv" div we created
             gifDiv.append(p);
@@ -91,19 +94,20 @@ $(document).ready(function () {
         }
       
 
-        $("#gifs-appear-here").on("click", function () {
+        $("img").on("click", function () {
           // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-          var state = $(this).personImageAnimate;
+          var state = $(this).attr("data-state");
           // If the clicked image's state is still, update its src attribute to what its data-animate value is.
           // Then, set the image's data-state to animate
           // Else set src to the data-still value
-          if (state === personImage) {
-            $(this).attr("src", $(this).personImageAnimate);
-            gifDiv.append(personImageAnimate);
+          if (state === "still") {
+            $(this).attr("data-state","animate");
+            $(this).attr("src", $(this).attr("data-animate"));
+            // gifDiv.append(personImageAnimate);
             $("#gifs-appear-here").append(gifDiv);
           } else {
-            $(this).attr("src", $(this).personImage);
-
+            $(this).attr("data-state","still");
+            $(this).attr("src", $(this).attr("data-still"));
           }
         });
 
